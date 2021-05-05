0e985e6f8166b57181e14a53b3dcd853631de562
Bruno Pivoto <brunorangel@gec.inatel.br>
2021-05-05 11:27:04 -0300

    secret key fixed

--- InvestmentMonitor/settings.py
+++ InvestmentMonitor/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('/secret_key.txt') as f:
+import os
+with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
